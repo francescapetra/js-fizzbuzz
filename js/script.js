453cab78e681efa49dd2659ffaca2071f8ba48f6
francescapetra <francescalaurapetra@gmail.com>
2021-03-02 15:52:53 +0100

    corretta indentazione

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 for (var i = 1; i <=100; i++) {
 // console.log(i);
 
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-if ((i % 3 == 0) && (i % 5 == 0))  {
-  console.log ("fizzbuzz");
-}
-// // ma per i multipli di 3 stampi “Fizz” al posto del numero
-else if (i % 3 == 0) {
-  console.log ("fizz");
+  // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+  if ((i % 3 == 0) && (i % 5 == 0)){
+    console.log ("fizzbuzz");
 
-// e per i multipli di 5 stampi Buzz.
-}else if (i % 5 == 0) {
-  console.log ("buzz");
+  // // ma per i multipli di 3 stampi “Fizz” al posto del numero
+  }else if (i % 3 == 0) {
+    console.log ("fizz");
 
-}else {
-  console.log(i);
-}
+  // e per i multipli di 5 stampi Buzz.
+  }else if (i % 5 == 0) {
+    console.log ("buzz");
+
+  }else {
+    console.log(i);
+  }
 }
